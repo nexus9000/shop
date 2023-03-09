8520ff87cde777219cc6840d95f0c08750b42af5
User <java.user.tester@gmail.com>
2023-03-09 16:50:05 +0200

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,12 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+L
+.idea/codeStyles/Project.xml,6\e\6ecd6000a7b6f4a2884412ff19f74193ed089648
+T
+$.idea/codeStyles/codeStyleConfig.xml,d\d\dd7b3350c29c49497756c7f32c69a3bed18b1cba
+\
+,src/main/java/edu/nbu/projectshop/Items.java,9\f\9f75b71ddabdbf87c28eb92752b6ddee468d4bcb
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- src/test/java/edu/nbu/projectshop/FoodItemsFactoryTest.java
+++ src/test/java/edu/nbu/projectshop/FoodItemsFactoryTest.java
@@ -1,14 +1,24 @@
 package edu.nbu.projectshop;
 
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FoodItemsFactoryTest {
-
+    private List<String> foodLists;
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        foodLists = Arrays.asList("milk", "meat", "bread");
     }
 
     @org.junit.jupiter.api.Test
     void createInstance() {
+        FoodItemsFactory fif = new FoodItemsFactory(foodLists);
+        Milk milk = (Milk) fif.createInstance("milk");
+        Assertions.assertTrue(Items.class.isAssignableFrom(Milk.class));
+        Assertions.assertTrue(milk instanceof Items);
     }
 }
