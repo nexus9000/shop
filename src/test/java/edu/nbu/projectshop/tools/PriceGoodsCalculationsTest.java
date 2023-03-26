bd26a8ee5aa61ae1585a3a7095b18d7a1c661f99
User <java.user.tester@gmail.com>
2023-03-26 14:01:49 +0300

    added test maximum markup percentage

--- src/test/java/edu/nbu/projectshop/tools/PriceGoodsCalculationsTest.java
+++ src/test/java/edu/nbu/projectshop/tools/PriceGoodsCalculationsTest.java
@@ -1,5 +1,6 @@
 package edu.nbu.projectshop.tools;
 
+import edu.nbu.projectshop.exceptions.TooBigMarkupException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,12 +14,14 @@ import java.util.Date;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PriceGoodsCalculationsTest {
-private final String productionDate = "2023-02-02";
-private final BigDecimal deliveryPrice = BigDecimal.valueOf(10.00);//real price
-private final BigDecimal percentage = BigDecimal.valueOf(0.2);//markup 20%
-private Date prDate;//date of production
-private Date currentDate;
-private PriceGoodsCalculations pgc;
+    private final String productionDate = "2023-02-02";
+    private final BigDecimal deliveryPrice = BigDecimal.valueOf(10.00);//real price
+    private final BigDecimal percentage = BigDecimal.valueOf(0.2);//markup 20%
+    private final BigDecimal MAXIMUM_MARKUP_PERCENTAGE = BigDecimal.valueOf(0.5);//maximum markup percentage
+    private Date prDate;//date of production
+    private Date currentDate;
+    private PriceGoodsCalculations pgc;
+
     @BeforeEach
     void setUp() throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
@@ -29,30 +32,41 @@ private PriceGoodsCalculations pgc;
 
     @Test
     void priceCalculation() {
-        assertEquals(200.7005, pgc.priceCalculation(deliveryPrice, percentage));
+        assertEquals(BigDecimal.valueOf(200.7005), pgc.priceCalculation(deliveryPrice, percentage));
     }
 
     @Test
     @DisplayName("check expiration date")
     void checkExpirationDate() {
         boolean markExpired = pgc.checkExpirationDate(currentDate, prDate, 3);
-        boolean markNotExpired = pgc.checkExpirationDate(currentDate, prDate,100);
+        boolean markNotExpired = pgc.checkExpirationDate(currentDate, prDate, 100);
         assertFalse(markExpired);
         assertTrue(markNotExpired);
     }
+
+    @Test
+    @DisplayName("test maximum markup percentage")
+    void testMarkupTooBigException() {
+        TooBigMarkupException exception = Assertions.assertThrows(TooBigMarkupException.class, () -> {
+            if (percentage.compareTo(MAXIMUM_MARKUP_PERCENTAGE) > 0)
+                throw new TooBigMarkupException("Too big markup. " + "Should less than 50 percent");
+        });
+        assertEquals("Too big markup. Should less than 50 percent", exception.getMessage());
+    }
+
     @Test
     @DisplayName("Test price item with expired period")
-    void testPriceItem()throws RuntimeException{
-      RuntimeException thrown = Assertions.assertThrows(RuntimeException.class,()->
-                pgc.calculateRealPrice(currentDate, prDate,30)  );
-        assertEquals("Expired product",thrown.getMessage());
+    void testPriceItem() throws RuntimeException {
+        RuntimeException thrown = Assertions.assertThrows(RuntimeException.class, () -> pgc.calculateRealPrice(currentDate, prDate, 30));
+        assertEquals("Expired product", thrown.getMessage());
     }
+
     @Test
     @DisplayName("Test price item with discount")
-    void testDiscountPrice()throws RuntimeException{
+    void testDiscountPrice() throws RuntimeException {
         int period = 60;//product  in days
         BigDecimal priceDiscount = pgc.calculateRealPrice(currentDate, prDate, period);
         //initial price is 10 markup percentage is 20 % so expected price is 12.00
-        assertEquals(12.00, priceDiscount);
+        assertEquals(BigDecimal.valueOf(12.00), priceDiscount);
     }
 }
