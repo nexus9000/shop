2582dc14f75ccaa4421e5c0d40b5492e5679336a
User <java.user.tester@gmail.com>
2023-04-06 17:16:32 +0300

    SaleOps.java

--- src/test/java/edu/nbu/projectshop/operations/SaleOpsTest.java
+++ src/test/java/edu/nbu/projectshop/operations/SaleOpsTest.java
@@ -1,12 +1,49 @@
 package edu.nbu.projectshop.operations;
 
+import edu.nbu.projectshop.employee.Cashier;
+import edu.nbu.projectshop.factories.NonFoodItemFactory;
+import edu.nbu.projectshop.goods.Items;
+import edu.nbu.projectshop.goods.NonFoodItems;
+import edu.nbu.projectshop.stores.NonFoodStore;
+import edu.nbu.projectshop.tools.GenerateIdNumber;
+import edu.nbu.projectshop.tools.Receipt;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DisplayName("Test sale ops")
 class SaleOpsTest {
-
+    private  Cashier cashier;
+    private SaleOps saleOps;
+    private  HashMap<Items, HashMap<BigDecimal, Integer>> listPurchased;
+    private  NonFoodStore nonFoodStore;
+    private NonFoodItems nails;
+    private NonFoodItems socks;
     @BeforeEach
     void setUp() {
+        cashier = new Cashier(101, "John", "Smith", 1000.00);
+        listPurchased = new HashMap<>();
+        nails = NonFoodItemFactory.createInstance("nails");
+        nails.setQuantity(100);
+        nails.setIdNum(GenerateIdNumber.generateId());
+        socks = NonFoodItemFactory.createInstance("socks");
+        socks.setQuantity(1000);
+        socks.setIdNum(GenerateIdNumber.generateId());
+        HashMap <Integer, NonFoodItems> storeMap = new HashMap<>();
+        nonFoodStore = new NonFoodStore(storeMap);
+        nonFoodStore.addItem(nails.getIdNum(),nails);
+        nonFoodStore.addItem(socks.getIdNum(), socks);
+        saleOps = new SaleOps();
+
+    }
+
+    @Test
+    void testRecipt()throws Exception{
+        AtomicReference<Receipt> receipt = saleOps.generateReceipt();
     }
 }
