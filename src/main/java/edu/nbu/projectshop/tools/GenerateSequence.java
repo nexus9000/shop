22d7f51396a1e46cb769316ffdff58eb68f721b0
User <java.user.tester@gmail.com>
2023-04-09 11:30:07 +0300

    sonarlint fixed

--- src/main/java/edu/nbu/projectshop/tools/GenerateSequence.java
+++ src/main/java/edu/nbu/projectshop/tools/GenerateSequence.java
@@ -1,20 +1,26 @@
 package edu.nbu.projectshop.tools;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.Properties;
 
 public class GenerateSequence {
+    private GenerateSequence() {}
 
-    public static Optional<Integer> generateSeq(String propsName)throws IOException{
-        FileInputStream fis = new FileInputStream(propsName);
+    public static Optional<Integer> generateSeq(String propsName) throws IOException {
         Properties props = new Properties();
-        props.load(fis);
-        Integer seqN = Integer.parseInt(props.getProperty("sequence"));
-        fis.close();
-        props.setProperty("sequence", (seqN++).toString());
-        return Optional.ofNullable(Optional.ofNullable(seqN)
+        int seqN;
+        try (FileInputStream fis = new FileInputStream(propsName)) {
+            props.load(fis);
+            seqN = Integer.parseInt(props.getProperty("sequence"));
+            props.setProperty("sequence", String.valueOf(seqN++));
+        }
+        try (FileOutputStream fos = new FileOutputStream(propsName)) {
+            props.store(fos, "Updated sequence");
+        }
+        return Optional.ofNullable(Optional.of(seqN)
                 .orElseThrow(() -> new RuntimeException("Problem with sequence")));
     }
 }
