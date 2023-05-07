2efc76d68716f3864266e3cd80fad0aabc2dcf7e
User <java.user.tester@gmail.com>
2023-05-07 11:19:19 +0300

    ShopCashiers.java added

--- src/test/java/edu/nbu/projectshop/operations/SaleOpsTest.java
+++ src/test/java/edu/nbu/projectshop/operations/SaleOpsTest.java
@@ -9,7 +9,6 @@ import edu.nbu.projectshop.stores.NonFoodStore;
 import edu.nbu.projectshop.tools.GenerateIdNumber;
 import edu.nbu.projectshop.tools.GenerateSequence;
 import edu.nbu.projectshop.tools.Receipt;
-import org.apache.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,7 +16,6 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -29,31 +27,27 @@ import java.util.concurrent.atomic.AtomicReference;
 import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Test sale ops")
 class SaleOpsTest {
-    private final static Logger logger = Logger.getLogger(SaleOpsTest.class);
+
 
     private SaleOps saleOps;
-    private  HashMap<Items, HashMap<BigDecimal, Integer>> listPurchased;
-    private  NonFoodStore nonFoodStore;
-    private NonFoodItems nails;
-    private NonFoodItems socks;
-   private ShopCashiers shopCashiers;
+    private ShopCashiers shopCashiers;
 
     @BeforeEach
     void setUp() {
         Cashier cashier = new Cashier(101, "John", "Smith", 1000.00);
         Cashier cashier2 = new Cashier(102,"Peter", "Donovan" , 1100);
-        listPurchased = new HashMap<>();
-        nails = NonFoodItemFactory.createInstance("nails");
-        Objects.requireNonNull(nails).setPrice(new BigDecimal(10.00));
+        HashMap<Items, HashMap<BigDecimal, Integer>> listPurchased = new HashMap<>();
+        NonFoodItems nails = NonFoodItemFactory.createInstance("nails");
+        Objects.requireNonNull(nails).setPrice(new BigDecimal("10.00"));
         Objects.requireNonNull(nails).setQuantity(100);
         nails.setIdNum(GenerateIdNumber.generateId());
-        socks = NonFoodItemFactory.createInstance("socks");
+        NonFoodItems socks = NonFoodItemFactory.createInstance("socks");
         Objects.requireNonNull(socks).setQuantity(1000);
-        Objects.requireNonNull(socks).setPrice(new BigDecimal( 2.0588).setScale(3, RoundingMode.CEILING));
+        Objects.requireNonNull(socks).setPrice(new BigDecimal("2.0588").setScale(3, RoundingMode.CEILING));
         socks.setIdNum(GenerateIdNumber.generateId());
         HashMap <Integer, NonFoodItems> storeMap = new HashMap<>();
-        nonFoodStore = new NonFoodStore(storeMap);
-        nonFoodStore.addItem(nails.getIdNum(),nails);
+        NonFoodStore nonFoodStore = new NonFoodStore(storeMap);
+        nonFoodStore.addItem(nails.getIdNum(), nails);
         nonFoodStore.addItem(socks.getIdNum(), socks);
         HashMap<BigDecimal, Integer> purchasedQ1 = new HashMap<>();
         purchasedQ1.put(nails.getPrice(),10);
@@ -61,7 +55,7 @@ class SaleOpsTest {
         HashMap<BigDecimal, Integer> purchasedQ2 = new HashMap<>();
         purchasedQ2.put(socks.getPrice(),10);
         listPurchased.put(socks,purchasedQ2);
-        saleOps = new SaleOps(cashier,listPurchased,nonFoodStore);
+        saleOps = new SaleOps(cashier, listPurchased, nonFoodStore);
         HashMap<Integer, Cashier> listCashier = new HashMap<>();
         //init cashiers list
         listCashier.put(cashier.getId(),cashier);
@@ -71,7 +65,7 @@ class SaleOpsTest {
 
     @Test
     @DisplayName("list cashiers")
-    void testCashierList()throws Exception{
+    void testCashierList(){
        assertEquals(new Cashier(101, "John" ,"Smith", 1000),
                shopCashiers.getListCashier().get(101) );
        shopCashiers.showListCashies();
@@ -85,11 +79,14 @@ class SaleOpsTest {
 
         assertNotNull(receipt);
         File receiptsDir = new File("receipts");
-        if(!receiptsDir.exists())receiptsDir.mkdir();
+        receiptsDir.mkdir();
         assertTrue(receiptsDir.exists());
-        String fileName = "receipts/"+currentSeq.get() + "receipts";
-        Path path = Paths.get(fileName);
-        Files.write(path, receipt.toString().getBytes(StandardCharsets.UTF_8));
+        String fileName = null;
+        if(currentSeq.isPresent()) {
+            fileName = "receipts/" + currentSeq.get() + "receipts";
+        }
+        Path path = Paths.get(Objects.requireNonNull(fileName));
+        Files.writeString(path, receipt.toString());
         new GetReceiptsInfo(receiptsDir.getAbsolutePath()).getInfoAllReceipts();
     }
 }
